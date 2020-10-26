58b1b64f16601fd3635f934e84febdc91f1156c1
IntoUnknown <theintounknown@users.noreply.github.com>
2020-10-25 23:36:31 -0500

    Added EnhancedTable, HiddenField, install_default_styles for table

--- tk_demo.py
+++ tk_demo.py
@@ -1,206 +1,18 @@
+from copy import deepcopy
+from pprint import pprint
 import tkinter as tk
 import tkinter.ttk as ttk
 from tkinter import messagebox
-from intounknown_lib.tk_gui import EnhancedButton, EnhancedLabel, EnhancedEntry, EnhancedText, EnhancedCheckbox, ScrollFrame, EnhancedCombobox, EnhancedListbox
-
-
-class EnhancedTable(tk.Frame):
-    def __init__(self, parent, id_field, headers=None, data=None, input_args=None, *args, **kwargs):
-        super().__init__(parent, *args, **kwargs)
-        input_args = input_args or {}
-        data = data or []
-        self.last_data_set_type = 'initialize'      # or 'initialize' or 'data'
-
-        self.config(padx=10, pady=10)
-        self.id_field = id_field
-
-        self.form = []
-        self.frame = None
-
-        self.headers = headers
-        self.last_id = 0
-
-        for obj in data:
-            val = obj.get(self, id_field, 0)
-            if isinstance(val, int):
-                continue
-            self.last_id = max(self.last_id, val)
-
-        self.initializeData(data)
-
-    def get(self):
-        return self._collect_form()
-
-    def set(self, data):
-        return self.set_data()
-
-    def clear(self):
-        self.destroy()
-        self.last_data_set_type = 'initialize'
-
-    def initialize_data(self, data):
-        if self.last_data_set_type != 'initialize':
-            return
-
-        self.last_data_set_type = 'initialize'
-        self.destroy()
-        self._render(data)
-
-    def set_data(self, data):
-        if len(data) < 1:
-            return
-
-        self.last_data_set_type = 'data'
-        self.destroy()
-        self._render(data)
-
-    def _collect_form(self):
-        data = []
-        for row in self.form:
-            record = {}
-            for key, obj in row.items():
-                record[key] = obj.get()
-            data.append(record)
-        return data
-
-    def add(self, row):
-        if self.id_field not in row[self.id_field] == 0:
-            result = self._collect_form()
-
-            self.last_id += 1
-            row[self.id_field] = self.last_id
-            result.append(row)
-            self.set_data()
-            return
-
-        result = []
-        data = self._collect_form()
-        for obj in data:
-            if row[self.id_field] == obj[self.id_field]:
-                result.append(row)
-                continue
-            result.append(obj)
-        self.set_data(result)
-
-    def remove(self, obj_id):
-        result = []
-        data = self._collect_form()
-        for obj in data:
-            if obj[self.id_field] == obj_id:
-                continue
-            result.append(obj)
-
-        self.set_data(result)
-
-    def fetch(self, obj_id):
-        data = self._collect_form()
-        for obj in data:
-            if obj[self.id_field] == obj_id:
-                return obj
-        return None
-
-    def set_options(self, key, values=None):
-        values = values or []
-
-        for obj in self.headers:
-            if obj['key'] == key:
-                obj['input_class_args']['values'] = values
-                break
-
-        for row in self.form:
-            if key not in row:
-                continue
-            row[key].set_options(values)
-
-    def _render(self, data):
-        self.form = []
-
-        self.frame = tk.Frame(self, pady=1, padx=1)
-        col = 0
-        for config in self.headers:
-            lookup = config['key']
-            hide = config.get('hide')
-            if hide:
-                continue
-
-            title = config.get('text', '')
-            ttk.Label(self.frame, text=title, style='TableHeader.TLabel').grid(row=0, column=0, sticky=(tk.N + tk.W + tk.E + tk.S))
-            col += 1
-
-        row = 1
-        for obj in data:
-            form_record = {}
-            col = 0
-            for config in self.headers:
-                lookup = config['key']
-                title = config['text']
-                hide = config.get('hide', False)
-
-                input_class = config.get('input_class', None)
-                label_text = config.get('label_text', None)
-                input_class_args = config.get('input_class_args', {})
-                is_button = config.get('button', False)
-                button_command = config.get('button_command', None)
-
-                val = obj.get(lookup, None)
-
-                if is_button:
-                    container = tk.Frame(self.frame)
-
-                    def closure(passed_value, cmd):
-                        return lambda: cmd(passed_value)
-
-                    func = closure(val, button_command)
-
-                    item = ttk.Button(container, text=label_text, command=func, **input_class_args)
-                    item.grid(row=0, column=0, sticky=(tk.N + tk.W + tk.E + tk.S))
-                    container.configure(background='white', borderwidth=1, relief='groove')
-                    container.grid(row=row, column=col, sticky=(tk.N + tk.W + tk.E + tk.S))
-
-                elif hide:
-                    form_record[lookup] = HiddenField(val)
-                    continue
-
-                elif input_class == None:
-                    container = tk.Frame(self.frame)
-                    item = ttk.Label(container, text=val, style='TableField.TLabel')
-                    form_record[lookup] = HiddenField(val)
-                    item.grid(row=0, column=0, stick=(tk.N + tk.W + tk.E + tk.S))
-                    container.configure(background='white', borderwidth=1, relief='groove')
-                    container.grid(row=row,column=col,sticky=(tk.N + tk.W + tk.E + tk.S))
-                else:
-                    item = input_class(self.frame, label_text, **input_class_args)
-                    form_record[lookup] = item
-                    item.configure(background='white', borderwidth=1, relief='groove')
-                    item.grid(row=row,column=col,sticky=(tk.N + tk.W + tk.E + tk.S))
-                    # if a column is missing or None, setting the input will probably fail and error
-                    if val != None:
-                        item.set(val)
-                    # store for data getting
-                col += 1
-            self.form.append(form_record)
-            row += 1
-
-        self.frame.grid(row=0,column=0,sticky=(tk.N + tk.W))
-
-
-    def destroy(self):
-        if self.frame != None:
-            self.frame.grid_forget()
-            self.frame.destroy()
-            self.form = []
-            self.frame = None
-
-
-
-
+from intounknown_lib.tk_gui import install_default_styles, EnhancedButton, EnhancedLabel, EnhancedEntry, EnhancedText, EnhancedCheckbox, ScrollFrame, EnhancedCombobox, EnhancedListbox, HiddenField, EnhancedTable
 
 
 class Application(tk.Tk):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.title("TK Demo")
-        self.geometry("700x400")
+        self.geometry("700x600")
+
+        install_default_styles()        # load default style for EnhancedTable
 
         # Setup content area
         self.main_menu = tk.Frame(self)
@@ -367,6 +179,75 @@ class Application(tk.Tk):
             .grid(row=6, column=0, sticky=(tk.N + tk.W))
 
 
+        ### Table Frame testing ###
+
+        table_frame = ScrollFrame(self.tabs, height=500, width=500)
+        self.table = None
+
+        def cb_click_fetch(obj_id):
+            print('btn_cb.fetch.id', obj_id)
+            print('cb_click_fetch:', self.table.fetch(obj_id))
+
+        def cb_click_remove(obj_id):
+            print('cb_click_remove', obj_id)
+            self.table.remove(obj_id)
+
+        categories = [
+            ('one', 'One'),
+            ('two', 'Two'),
+            ('three', 'Three'),
+            ('four', 'Four'),
+        ]
+
+        headers = [
+            {'key': 'id', 'text': 'ID', 'hide': False},
+            {'key': 'name', 'text': 'Name'},
+            {'key': 'category', 'text': 'Category', 'input_class': EnhancedCombobox, 'input_class_args': {'values': categories}, 'hide': False},
+            {'key': 'id', 'button': True, 'text': 'Fetch', 'label_text': 'Fetch', 'button_command': cb_click_fetch},
+            {'key': 'id', 'button': True, 'text': 'Remove', 'label_text': 'Remove', 'button_command': cb_click_remove},
+        ]
+
+        data = [
+            {'id': 1, 'name': 'Orange', 'category': 'one', },
+            {'id': 2, 'name': 'Blue', 'category': 'one', },
+            {'id': 3, 'name': 'Green', 'category': 'two', },
+            {'id': 4, 'name': 'Red', 'category': 'two', },
+        ]
+
+        data2 = deepcopy(data)
+        data2.append({'id': 5, 'name': 'Yellow', 'category': 'three'})
+        data2.append({'id': 6, 'name': 'Purple', 'category': 'three'})
+        data2.append({'id': 7, 'name': 'white', 'category': 'three'})
+
+
+        self.table = EnhancedTable(table_frame.viewport, 'id', headers=headers, data=data)
+        #self.table.set_options('categories', values=categories)
+        self.table.grid(row=0, column=0, sticky=(tk.N + tk.S, tk.E, tk.W))
+        self.table.rowconfigure(0, weight=3)
+        self.table.columnconfigure(0, weight=3)
+
+
+        EnhancedButton(table_frame.viewport, 'Serialize Table', command=lambda: print(self.table.get())) \
+            .grid(row=1, column=0, sticky=(tk.N + tk.W))
+
+        EnhancedButton(table_frame.viewport, 'Update Table Data', command=lambda: self.table.set(data2)) \
+            .grid(row=2, column=0, sticky=(tk.N + tk.W))
+
+
+
+
+        self.new_table_entry = EnhancedEntry(table_frame.viewport, 'Input:', label_args={'width': 10})
+        self.new_table_entry.grid(row=3, column=0, sticky=(tk.W + tk.N))
+
+        def create_new_table_row():
+            val = self.new_table_entry.get()
+            self.table.add(
+                {'id': 0, 'name': val, 'category': 'four'},
+            )
+
+        EnhancedButton(table_frame.viewport, 'Add New Record', command=create_new_table_row) \
+            .grid(row=4, column=0, sticky=(tk.N + tk.W))
+
 
         self.tabs.grid(row=0, column=0, sticky=(tk.N, tk.W + tk.S + tk.E))
         self.tabs.add(entry_frame, text='Entry')
@@ -375,6 +256,7 @@ class Application(tk.Tk):
         self.tabs.add(scrolling_frame, text='Scrolling Frame')
         self.tabs.add(combobox_frame, text='Combobox')
         self.tabs.add(listbox_frame, text='Listbox')
+        self.tabs.add(table_frame, text='Table')
 
         #content.grid(row=0, column=0, sticky=tk.W)
         self.main_menu.grid(row=0, column=0, sticky=(tk.N, tk.W))
